de22850cd40899ed839fa00b2e4227da8f2bebeb
JazRiv <110856832+JazRiv@users.noreply.github.com>
2023-07-14 15:39:53 -0600

    prueba

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function draw(){
 
         if (!cancion_1.isPlaying()){
             cancion_1.play();
-            setVolume(0.5);
+            cancion_1.setVolume(0.5);
             document.getElementById("song_name"). innerHTML = "Esta cancion se llama: As it was";
         }
     }
@@ -51,7 +51,7 @@ function draw(){
 
         if (!estado_2.isPlaying()){
             cancion_2.play();
-            setVolume(0.5);
+            cancion_2.setVolume(0.5);
             document.getElementById("song_name").innerHTML = "Esta cancion se llama: Never gonna give you up";
         }
     }
